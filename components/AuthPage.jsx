12c8c7a07c20e6a2df2bdbc4ae0e4bca53c476d0
Hawau <owonikokohawawuolanike@gmail.com>
2025-09-16 17:22:13 -0700

    update

--- components/AuthPage.jsx
+++ components/AuthPage.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import LoginPge from "./LoginPage";
 import ResgisterPage from "./RegisterPage";
+import FormImg from "../src/assets/Form image.png";
 
 export default function AuthPage() {
   const [activeTab, setActiveTab] = useState("login");
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex justify-center items-center bg-[url(../src/assets/Form-Bg.png)] bg-cover bg-center">
       <div className="flex flex-col border border-none md:border border-white flex-row h-fit w-fit rounded-lg">
         
+        <div>
+          <img src={FormImg} alt="Form Image" className="h-135 w-150" />
+        </div>
         <div className=" shadow-[0_4px_8px_rgba(0,0,0,0.2),_0_6px_20px_rgba(0,0,0,0.19)] sm:px-8 mx-auto sm:w-3/4 md:w-1/2 max-w-md  font-sans bg-white flex flex-col justify-center overflow-hidden rounded-r ">
           <div className=" flex items-center justify-center bg-gray-100">
             <div className="bg-white  w-full max-w-md p-8">
